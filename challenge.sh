5437b445ea37c10edc20fd00ab88f6ae43b5c738
Garrett M. <84481668+Garrett-M-code@users.noreply.github.com>
2025-09-07 21:38:26 -0400

    Add files via upload

---
+++ challenge.sh
@@ -0,0 +1,22 @@
+useradd -m hackerman
+useradd systemctlr
+
+sleep 5000 &
+sleep 5000 &
+sleep 5000 &
+sleep 5000 &
+sleep 5000 &
+sleep 5000 &
+sleep 5000 &
+sleep 5000 &
+
+
+clear
+
+echo "Challenge 1:"
+echo "#############################################################"
+echo ""
+echo "HAHAHA! You have fallen for my trap and installed some malicious users to your system!"
+echo "See if you can find the 2 evil users, and 8 hidden processes, and delete them."
+echo "Welp, after CONTROLLING you SYSTEM, i'm feeling SLEEPY. Later!"
+echo "-- Hackerman >;)"
